28179129268a85de0ab0c21be0ebb86fc1b48562
Rampatil1305 <ramanagoudsp13@gamil.com>
2025-04-09 10:54:59 +0530

    save

--- add.java
+++ add.java
@@ -3,7 +3,8 @@ public class add{
         int a = 5; // Initialize a with a value
         int b = 10; // Initialize b with a value
         add obj = new add();
-        
+
         System.out.println("Addition of a and b is: "+(a+b));
+        System.out.println("Subtraction of a and b is: "+obj.sub(a,b));
     }
 }
