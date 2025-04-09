ead0ae72b7882d1fbe747a7280cad5972e6407f7
Rampatil1305 <ramanagoudsp13@gamil.com>
2025-04-09 10:52:13 +0530

    save

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add{
+    public static void main(String args[]){
+        int a = 5; // Initialize a with a value
+        int b = 10; // Initialize b with a value
+        add obj = new add();
+        
+        System.out.println("Addition of a and b is: "+(a+b));
+    }
+}
